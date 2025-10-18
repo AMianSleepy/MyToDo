0ff7e7c674096dc57edfe70c644383c0efe4596d
ShiYi <AMian_Sleepy@qq.com>
2025-10-18 17:32:10 +0800

    添加项目文件。

---
+++ DailyApp.WPF/DailyApp.WPF/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using DailyApp.WPF.ViewModels;
+using DailyApp.WPF.Views;
+using Prism.DryIoc;
+using Prism.Ioc;
+using Prism.Services.Dialogs;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DailyApp.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        /// <summary>
+        /// 设置启动页
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWin>();
+        }
+
+        /// <summary>
+        /// 设置依赖注入
+        /// </summary>
+        /// <param name="containerRegistry"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // 登录
+            containerRegistry.RegisterDialog<LoginUC, LoginUCViewModel>();
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            var dialog = Container.Resolve<IDialogService>();
+            dialog.ShowDialog("LoginUC", callback =>
+            {
+                if (callback.Result != ButtonResult.OK)
+                {
+                    Environment.Exit(0);
+                    return;
+                }
+                base.OnInitialized();
+            });
+        }
+    }
+}
---
+++ DailyApp.WPF/DailyApp.WPF/ViewModels/LoginUCViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Prism.Commands;
+using Prism.Services.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyApp.WPF.ViewModels
+{
+    /// <summary>
+    /// 登录视图模型
+    /// </summary>
+    class LoginUCViewModel : IDialogAware// 实现对话框服务的接口
+    {
+        public string Title { get; set; } = "我的日常";
+
+        public event Action<IDialogResult> RequestClose;
+
+        /// <summary>
+        /// 登录命令
+        /// </summary>
+        public DelegateCommand LoginCmm { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public LoginUCViewModel()
+        {
+            LoginCmm = new DelegateCommand(Login);
+        }
+
+        /// <summary>
+        /// 登录方法
+        /// </summary>
+        private void Login()
+        {
+            // 模拟登录成功
+            RequestClose?.Invoke(new DialogResult(ButtonResult.OK));
+        }
+
+        /// <summary>
+        /// 是否能够关闭对话框
+        /// </summary>
+        /// <returns></returns>
+        public bool CanCloseDialog()
+        {
+            return true;
+        }
+
+        public void OnDialogClosed()
+        {
+            
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+            
+        }
+    }
+}
