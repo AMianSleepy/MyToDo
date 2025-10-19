4af7a021a8e6a05dd454d8fcd463c7390f5f1235
ShiYi <AMian_Sleepy@qq.com>
2025-10-19 23:08:42 +0800

    使用EF的CodeFirst代码优先模式：由实体类生成数据库表结构

---
+++ DailyApp/DailyApp.Api/DataModel/DaliyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DailyApp.Api.DataModel
+{
+    /// <summary>
+    /// 数据库上下文
+    /// </summary>
+    public class DaliyDbContext : DbContext
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="options"></param>
+        public DaliyDbContext(DbContextOptions<DaliyDbContext> options) : base (options)
+        {
+            
+        }
+
+        // 定义要迁移的数据模型
+        public DbSet<AccountInfo> AccountInfo { get; set; }
+
+    }
+}
---
+++ 垃圾箱/DailyApp.API/DTOs/AccountInfoDTO.cs
@@ -0,0 +1,24 @@
+﻿// Data Transger Object 数据传输对象
+namespace DailyApp.API.DTOs
+{
+    /// <summary>
+    /// 账号DTO（用来接收数据信息）
+    /// </summary>
+    public class AccountInfoDTO
+    {
+        /// <summary>
+        /// 用户名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 账户
+        /// </summary>
+        public string Account { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Pwd { get; set; }
+    }
+}
