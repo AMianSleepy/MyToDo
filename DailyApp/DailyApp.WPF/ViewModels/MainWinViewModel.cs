f2e52b72ae9795c3a58663710b549e2975c9ac8d
ShiYi <AMian_Sleepy@qq.com>
2025-10-29 21:39:16 +0800

    左侧菜单设计
    在应用程序中实现左侧菜单栏的开发，包括菜单模型的定义、菜单数据的绑定及UI样式的设计。课程内容覆盖了菜单图标和名称的配置，动态绑定数据的实现以及主题颜色背景的应用等。通过具体代码和示例，帮助学习者掌握基于MVVM模式的菜单创建与界面切换基础，为后续添加页面切换功能打下坚实基础。

--- DailyApp/DailyApp.WPF/ViewModels/MainWinViewModel.cs
+++ DailyApp/DailyApp.WPF/ViewModels/MainWinViewModel.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using DailyApp.WPF.Models;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace DailyApp.WPF.ViewModels
 {
-    class MainWinViewModel
+    /// <summary>
+    /// 主界面视图模型
+    /// </summary>
+    internal class MainWinViewModel : BindableBase
     {
+        #region 左侧菜单
+        private List<LeftMenuInfo> _LeftMenuList;
+
+        public List<LeftMenuInfo> LeftMenuList
+        {
+            get { return _LeftMenuList; }
+            set
+            { 
+                _LeftMenuList = value;
+                RaisePropertyChanged();
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public MainWinViewModel()
+        {
+            LeftMenuList = new List<LeftMenuInfo>();
+
+            // 创建菜单数据
+            CreateMenu();
+        }
+
+        /// <summary>
+        /// 创建菜单数据
+        /// </summary>
+        private void CreateMenu()
+        {
+            LeftMenuList.Add(new LeftMenuInfo() { Icon = "Home", MenuName = "首页", ViewName = "IndexView" });
+            LeftMenuList.Add(new LeftMenuInfo() { Icon = "NotebookOutline", MenuName = "待办事项", ViewName = "ToDoView" });
+            LeftMenuList.Add(new LeftMenuInfo() { Icon = "NotebookPlus", MenuName = "备忘录", ViewName = "MemoView" });
+            LeftMenuList.Add(new LeftMenuInfo() { Icon = "Cog", MenuName = "设置", ViewName = "SettingsView" });
+        }
     }
 }
