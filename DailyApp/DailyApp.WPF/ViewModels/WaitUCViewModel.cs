93e3114600c52e1ec2d404949b5964d1c3665fd5
ShiYi <AMian_Sleepy@qq.com>
2025-11-01 18:00:00 +0800

    待办页显示设计
    新增待办事项视图模型及用户界面
    
    - 在 WaitUCViewModel.cs 中新增待办事项数据模型：
      - 添加命名空间引用 DailyApp.WPF.DTOs 和 Prism.Mvvm。
      - 新增 WaitList 属性及 CreateWaitList 方法，生成模拟数据。
      - 使用 BindableBase 支持数据绑定。
    - 在 WaitUC.xaml 中设计待办事项用户界面：
      - 使用 ItemsControl 和 WrapPanel 显示待办事项卡片。
      - 添加筛选功能（TextBox 和 ComboBox）。
      - 集成 Material Design 控件（PopupBox）。
      - 优化布局，支持动态数据绑定。

--- DailyApp/DailyApp.WPF/ViewModels/WaitUCViewModel.cs
+++ DailyApp/DailyApp.WPF/ViewModels/WaitUCViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DailyApp.WPF.DTOs;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,54 @@ using System.Threading.Tasks;
 
 namespace DailyApp.WPF.ViewModels
 {
-    internal class WaitUCViewModel
+
+    /// <summary>
+    /// 待办事项视图模型
+    /// </summary>
+    internal class WaitUCViewModel : BindableBase
     {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public WaitUCViewModel()
+        {
+            CreateWaitList();
+        }
+
+        private List<DailyApp.WPF.DTOs.WaitInfoDTO> _WaitList;
+        /// <summary>
+        /// 待办事项数据
+        /// </summary>
+        public List<DailyApp.WPF.DTOs.WaitInfoDTO> WaitList
+        {
+            get { return _WaitList; }
+            set
+            {
+                _WaitList = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// 创建待办事项模拟数据
+        /// </summary>
+        private void CreateWaitList()
+        {
+            WaitList = new List<WaitInfoDTO>
+            {
+                new WaitInfoDTO() { Title = "测试录屏", Content = "仔细Content"},
+                new WaitInfoDTO() { Title = "上传录屏", Content = "1234567890"},
+                new WaitInfoDTO() { Title = "测试录屏", Content = "仔细Content"},
+                new WaitInfoDTO() { Title = "上传录屏", Content = "1234567890"},
+                new WaitInfoDTO() { Title = "测试录屏", Content = "仔细Content"},
+                new WaitInfoDTO() { Title = "上传录屏", Content = "1234567890"},
+                new WaitInfoDTO() { Title = "测试录屏", Content = "仔细Content"},
+                new WaitInfoDTO() { Title = "上传录屏", Content = "1234567890"},
+                new WaitInfoDTO() { Title = "测试录屏", Content = "仔细Content"},
+                new WaitInfoDTO() { Title = "上传录屏", Content = "1234567890"},
+                new WaitInfoDTO() { Title = "测试录屏", Content = "仔细Content"},
+                new WaitInfoDTO() { Title = "上传录屏", Content = "1234567890"},
+            };
+        }
     }
 }
