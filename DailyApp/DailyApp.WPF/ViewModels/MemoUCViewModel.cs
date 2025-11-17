c01df4b3ce582df437e94651b5054a738d7c664a
ShiYi <AMian_Sleepy@qq.com>
2025-11-17 15:28:44 +0800

    查询显示备忘录
    重构备忘录查询逻辑并更新UI绑定
    
    在 MemoUCViewModel.cs 中引入 HttpRestClient 以替代模拟数据，重构了备忘录查询逻辑。新增 SearchTitle 属性和 QueryMemoListCmm 命令以支持动态查询。在 LoginUC.xaml 和 MemoUC.xaml 中更新了 TextBox 的绑定设置，确保数据源在文本更改时即时更新，并添加了按下 Enter 键触发查询命令的功能。

--- DailyApp/DailyApp.WPF/ViewModels/MemoUCViewModel.cs
+++ DailyApp/DailyApp.WPF/ViewModels/MemoUCViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using DailyApp.WPF.DTOs;
+using DailyApp.WPF.HttpClients;
 using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,14 +19,19 @@ namespace DailyApp.WPF.ViewModels
         /// <summary>
         /// 构造函数
         /// </summary>
-        public MemoUCViewModel()
+        public MemoUCViewModel(HttpRestClient _httpRestClient)
         {
-            CreateMemoList();
+            httpRestClient = _httpRestClient;
+
+            QueryMemoList();
 
             // 显示添加备忘录命令
             ShowAddMemoCmm = new DelegateCommand(ShowAddMemo);
+            // 查询备忘录数据
+            QueryMemoListCmm = new DelegateCommand(QueryMemoList);
         }
 
+        #region 
         private List<DailyApp.WPF.DTOs.MemoInfoDTO> _MemoList;
         /// <summary>
         /// 备忘录事项数据
@@ -40,38 +47,47 @@ namespace DailyApp.WPF.ViewModels
         }
 
         /// <summary>
-        /// 创建备忘录事项模拟数据
+        /// 查询标题
         /// </summary>
-        private void CreateMemoList()
+        public string SearchTitle { get; set; }
+
+        /// <summary>
+        /// 客户端
+        /// </summary>
+        private readonly HttpRestClient httpRestClient;
+
+        /// <summary>
+        /// 查询备忘录事项数据
+        /// </summary>
+        private void QueryMemoList()
         {
             MemoList = new List<MemoInfoDTO>
             {
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
-                new MemoInfoDTO() { Title = "会议一", Content = "仔细Content"},
-                new MemoInfoDTO() { Title = "会议二", Content = "1234567890"},
+                
             };
+            ApiRequest apiRequest = new()
+            {
+                Method = RestSharp.Method.GET,
+                Route = $"Memo/QueryMemo?title={SearchTitle}",
+            };
+
+            ApiResponse response = httpRestClient.Execute(apiRequest);
+
+            if (response.ResultCode == 1)
+            {
+                MemoList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<MemoInfoDTO>>(response.ResultData.ToString());
+            }
+            else
+            {
+                MemoList = new List<MemoInfoDTO>();
+            }
         }
+
+        /// <summary>
+        /// 设置查询备忘录列表的命令
+        /// </summary>
+        public DelegateCommand QueryMemoListCmm { get; set; }
+        #endregion
 
         #region 显示“添加备忘录”
         private bool _IsShowAddMemo;
